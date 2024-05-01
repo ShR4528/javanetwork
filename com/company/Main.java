bc514389245d83db797413de3e8a4c73c026b9a5
Shr4528 <Shr4528@gmail.com>
2024-05-01 14:38:27 -0500

    Update Main.java to use URL class instead of URI; remove Main3.java and Main4.java
    The changes include:
    - Main.java: modified to use URL class instead of URI to print details of a URL.

--- com/company/Main.java
+++ com/company/Main.java
@@ -1,32 +1,22 @@
 package com.company;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 
+public class Main {
+    public static void main(String[] args) throws MalformedURLException{
+        String web = "https://en.wikipedia.org/wiki/URL#Citations";
 
-import java.net.URI;
-import java.net.*;
-import java.net.URISyntaxException;
-import java.net.UnknownHostException;
+        URL myUrl = new URL(web);
 
-
-public class Main{
-    public static void  main(String[] args) throws UnknownHostException, URISyntaxException {
-
-        String website = "https://techbiz.com/ecu-ecm-power-management/";
-        URI webURI = new URI("https","techbinz.com","ecu-ecm-power-management");
-        getURI(webURI);
-    }
-
-
-    static void getURI(URI myUri){
-        System.out.println(myUri.getAuthority());
-        System.out.println(myUri.getPath());
-        System.out.println(myUri.getHost());
-        System.out.println(myUri.getPort());
-        System.out.println(myUri.getScheme());
-        System.out.println(myUri.getQuery());
+        getURL(myUrl);
 
 
     }
-    
+    static void getURL(URL url){
+        System.out.println("PATH = " +  url.getPath());
+        System.out.println("Host = " + url.getHost());
+        System.out.println("REF = " + url.getRef());
+        
+    }
 }
- 
