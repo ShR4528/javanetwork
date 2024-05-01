121d177737d76eb913169443e214aea95fb5f44f
Shr4528 <Shr4528@gmail.com>
2024-05-01 10:04:48 -0500

    Delete Main.java, update Main2.java: remove unused variable, add comments and ByteBuffer for reading content
    The commit removes the Main.java file and updates the Main2.java file. In Main2.java, the

--- Main2.java
+++ Main2.java
@@ -6,29 +6,42 @@ import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 
+/**
+ * Этот класс демонстрирует, как получить содержимое веб-страницы с использованием URLConnection в Java.
+ */
 public class Main2 {
-     private static  ByteBuffer buffer;
- 
+    private static ByteBuffer buffer;
+
     public static void main(String[] args) {
         try {
+            // Определяем URL веб-страницы для получения
             URL url = new URL("https://ocsaly.com");
+
+            // Открываем соединение с указанным URL
             URLConnection urlConnection = url.openConnection();
 
+            // Получаем InputStream из соединения URL
             InputStream inputStream = urlConnection.getInputStream();
+
+            // Создаем ReadableByteChannel из InputStream
             ReadableByteChannel readableByteChannel = Channels.newChannel(inputStream);
+
+            // Выделяем ByteBuffer для считывания данных
             ByteBuffer buffer = ByteBuffer.allocate(64);
 
-            String line = null;
-            while (readableByteChannel.read(buffer) > 0){
+            // Считываем данные из ReadableByteChannel в ByteBuffer
+            // и выводим их в консоль
+            while (readableByteChannel.read(buffer) > 0) {
                 System.out.println(new String(buffer.array()));
                 buffer.clear();
             }
-            
+
+            // Закрываем ReadableByteChannel
             readableByteChannel.close();
-        
 
         } catch (IOException ioException) {
-         System.out.println(ioException.getMessage());
+            // Обрабатываем любые исключения ввода-вывода, которые могут возникнуть в процессе
+            System.out.println(ioException.getMessage());
         }
-}
+    }
 }
