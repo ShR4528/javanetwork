2554544490e554548a4f20010e3a6dceae0a8787
Shr4528 <Shr4528@gmail.com>
2024-05-06 19:05:37 -0500

    "Added input/output handling and message to client in server program"
    The changes made include:
    
    * Importing necessary classes for the program
    * Defining the main class for the program
    * Creating a ServerSocket object with

--- com/company/Main.java
+++ com/company/Main.java
@@ -1,21 +1,35 @@
+// Импортируйте необходимые классы для программы
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
+// Определите главный класс для программы
 public class Main {
-    public static void main(String[] args) throws IOException{
+
+    // Определите главный метод для программы
+    public static void main(String[] args) throws IOException {
+
+        // Создайте новый объект ServerSocket с номером порта 1092
         ServerSocket serverSocket = new ServerSocket(1092);
 
-
+        // Примите соединение от клиента и сохраните его в переменной link
         Socket link = serverSocket.accept();
+
+        // Создайте новый объект Scanner для чтения ввода от клиента
         Scanner input = new Scanner(link.getInputStream());
+
+        // Создайте новый объект PrintWriter для отправки вывода клиенту
         PrintWriter output = new PrintWriter(link.getOutputStream());
 
-        output.println("awaiting for data ...");
+        // Отправьте сообщение клиенту, указывающее, что сервер ожидает данных
+        output.println("awaiting for data...");
+
+        // Прочитайте строку ввода от клиента и сохраните ее в переменной myinput
         String myinput =  input.nextLine();
 
+        // Закройте соединение с клиентом
         link.close();
-     }
+    }
 }
