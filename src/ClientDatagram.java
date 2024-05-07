cce1a2b1ec85d4567209e7ec8f17938d4acc47ea
Shr4528 <Shr4528@gmail.com>
2024-05-07 14:46:16 -0500

    Added functionality for creating a datagram socket, getting local host, and creating a loop for sending and receiving messages with a scanner for user input. Exiting loop with "CLOSE" message. UnknownHostException and general exceptions caught and

--- src/ClientDatagram.java
+++ src/ClientDatagram.java
@@ -1,7 +1,41 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class ClientDatagram {
-    private static final int PORT = 2082;
 
-    private InetAddress host;
+    private static final int PORT = 2082;
+    private static DatagramPacket inPacket, outPacket;
+    private static DatagramSocket datagramSocket;
+    private static InetAddress host;
+
+    private byte[] buffer;
+
+    public static void main(String[] args) {
+    try {
+        host = InetAddress.getLocalHost();
+    } catch (UnknownHostException unknownHostException) {
+        System.out.println("Error : " + unknownHostException.getMessage());
+        System.exit(1);
+    }
+    accessServer();
+
+    }
+    private static void accessServer(){
+        try {
+            datagramSocket = new DatagramSocket();
+            Scanner userEntry = new Scanner(System.in);
+            String message = "";
+            String response = "";
+
+            do {
+
+            } while(!message.equals("CLOSE"));  
+
+        } catch (Exception e) {
+            
+        }
+    }
 }
